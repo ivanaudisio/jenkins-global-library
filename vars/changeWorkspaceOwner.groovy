eb8d10078ab459eddd9d2c9887682fad6f15a24c
Ivan Audisio <ivan.audisio@mail.com>
2017-11-12 21:50:37 +0000

    increased access

--- vars/changeWorkspaceOwner.groovy
+++ vars/changeWorkspaceOwner.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String newOwner) {
-  sh "sudo chmod -R 775 $WORKSPACE"
+  sh "sudo chmod -R 777 $WORKSPACE"
   sh "sudo chown -R ${newOwner}:${newOwner} $WORKSPACE"
 }
