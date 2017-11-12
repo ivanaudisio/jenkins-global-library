41351d651a916791c4cbc7a4e7969d91e937601a
Ivan Audisio <ivan.audisio@mail.com>
2017-11-12 22:22:50 +0000

    fixed compress command

--- vars/compressFolder.groovy
+++ vars/compressFolder.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String folderPath) {
-  sh "tar -xvzf ${folderPath}.tar.gz ${folderPath}"
+  sh "tar -cvzf ${folderPath}.tar.gz ${folderPath}"
   sh "rm -fR ${folderPath}"
 }
